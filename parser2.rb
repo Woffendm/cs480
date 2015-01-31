735cd5b7eaea5d1159378301962a5b8ced4ce8f1
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2015-01-30 18:01:42 -0800

    Add exception handeling

--- parser2.rb
+++ parser2.rb
@@ -102,8 +102,8 @@ end
 
 
 class ParseException < Exception  
-  def initialize line=0, index=0, char='', msg=''
-    super "Unexpected character '#{char}' on #{line}:#{index}. #{msg}"
+  def initialize line=0, index=0, str='', msg=''
+    super "Unexpected character '#{str[index]}' on #{line}:#{index}. #{msg}"
   end
 end
 
@@ -113,7 +113,7 @@ end
 class Parser
 
 # Note that 'next' is the same as repeating the current character. 
-def self.parse string
+def self.parse string, nofail=true, quite=true
   # Turn that mofo into an array
   line = 0
   index = 0
@@ -121,104 +121,176 @@ def self.parse string
   token = nil
   tokens = []
   while index <= string.length
-    puts index
-    char = string[index]
-    next_char = string[index + 1]
-    case token
-    when nil
-      case char
-      when '+'
-        tokens << Operator.new(char)
-      when '-'
-        tokens << Operator.new(char)
-      when '*'
-        tokens << Operator.new(char)
-      when '/'
-        tokens << Operator.new(char)
-      when '%'
-        tokens << Operator.new(char)
-      when '^'
-        tokens << Operator.new(char)
-      when '='
-        tokens << Comparison.new(char)
+    begin
+      char = string[index]
+      next_char = string[index + 1]
+      case token
+      when nil
+        case char
+        when nil
+          break
+        when ' '
+          # do nothing
+        when '+'
+          tokens << Operator.new(char)
+        when '-'
+          tokens << Operator.new(char)
+        when '*'
+          tokens << Operator.new(char)
+        when '/'
+          tokens << Operator.new(char)
+        when '%'
+          tokens << Operator.new(char)
+        when '^'
+          tokens << Operator.new(char)
+        when '='
+          tokens << Comparison.new(char)
+        when '>'
+          token = char
+        when '<'
+          token = char
+        when ':'
+          token = char
+        when '('
+          tokens << Token.new(char)
+        when ')'
+          tokens << Token.new(char)
+        when '!'
+          token = char
+        when '"'
+          token = char
+        when 'a'
+          token = char
+        when 'b'
+          token = char
+        when 'c'
+          token = char
+        when 'f'
+          token = char
+        when 'i'
+          token = char
+        when 'n'
+          token = char
+        when 'o'
+          token = char
+        when 's'
+          token = char
+        when 't'
+          token = char
+        when 'w'
+          token = char
+        else
+          throw ParseException.new line, index, char
+        end
       when '>'
-        token = char
+        case char
+        when '='
+          tokens << Comparison.new(token + char)
+          token = nil
+        else
+          tokens << Comparison.new(token)
+          token = nil
+          next
+        end
       when '<'
-        token = char
-      when ':'
-        token = char
-      when '('
-        tokens << Token.new(char)
-      when ')'
-        tokens << Token.new(char)
+        case char
+        when '='
+          tokens << Comparison.new(token + char)
+          token = nil
+        else
+          tokens << Comparison.new(token)
+          token = nil
+          next
+        end
       when '!'
-        token = char
-      when '"'
-        token = char
+        case char
+        when '='
+          tokens << Comparison.new(token + char)
+          token = nil
+        else
+          tokens << Comparison.new(token)
+          token = nil
+          next
+        end
       when 'a'
-        token = char
+        case char
+        when 'n'
+          token += char
+        else
+          throw ParseException.new line, index - 1, string
+          token = nil
+          next
+        end
+      when 'an'
+        case char
+        when 'd'
+          tokens << Comparison.new(token + char)
+          token = nil
+        else
+          throw ParseException.new line, index - 2, string
+          index -= 1
+          token = nil
+          next
+        end
       when 'b'
-        token = char
+        case char
+        when 'o'
+          token += char
+        else
+          throw ParseException.new line, index - 1, string
+          token = nil
+          next
+        end
+      when 'bo'
+        case char
+        when 'o'
+          token += char
+        else
+          throw ParseException.new line, index - 2, string
+          token = nil
+          next
+        end
+      when 'boo'
+        case char
+        when 'l'
+          tokens << Type.new(token + char)
+          token = nil
+        else
+          throw ParseException.new line, index - 3, string
+          index -= 2
+          token = nil
+          next
+        end
       when 'c'
-        token = char
-      when 'f'
-        token = char
-      when 'i'
-        token = char
-      when 'n'
-        token = char
-      when 'o'
-        token = char
-      when 's'
-        token = char
-      when 't'
-        token = char
-      when 'w'
-        token = char
-      else
-        ParseException.new line, index, char
-      end
-    when '>'
-      case char
-      when '='
-        tokens << Comparison.new(token + char)
-        token = nil
-      else
-        tokens << Comparison.new(token)
-        token = nil
-        next
-      end
-    when '<'
-      case char
-      when '='
-        tokens << Comparison.new(token + char)
-        token = nil
-      else
-        tokens << Comparison.new(token)
-        token = nil
-        next
-      end
-    when '='
-      case char
-      when '='
-        tokens << Comparison.new(token + char)
-        token = nil
-      else
-        tokens << Comparison.new(token)
-        token = nil
-        next
-      end
-    when 'a'
-      case char
-      when 'n'
-        token += char
-      else
-        ParseException.new line, index - 1, token
-        token = nil
-        next
-      end
+        case char
+        when 'o'
+          token += char
+        else
+          throw ParseException.new line, index - 1, string
+          token = nil
+          next
+        end
+      when 'co'
+        case char
+        when 's'
+          tokens << Comparison.new(token + char)
+          token = nil
+        else
+          throw ParseException.new line, index - 2, string
+          index -= 1
+          token = nil
+          next
+        end
       
-      
+        
+      end
+    # Rescue parse errors or let them DIE
+    rescue Exception => e
+      if nofail
+        puts e unless quite
+      else
+        raise e
+      end
     end
     
     index += 1
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,9 +1,87 @@
 require_relative '../parser2'
 
 describe Parser do
-  it 'can parse operators' do
-    data = "+-*^%/<<=>>==!="
-    expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
-    to eq("+ - * ^ % / < <= > >= = !=")
+  context 'can parse:' do
+  
+    context 'operators:' do
+      it 'unique and valid' do
+        data = "+-*^%/<<=>>==!="
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("+ - * ^ % / < <= > >= = !=")
+      end
+      
+      it 'multiple' do
+        data = "++ +"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("+ + +")
+      end      
+    end
+    
+    
+    
+    context 'and:' do
+      it 'just one' do
+        data = "and"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("and")
+      end
+    
+      it 'multiple' do
+        data = "andand and"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("and and and")
+      end
+      
+      it 'with partials' do
+        data = "aanandanr"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("and")
+      end
+    end
+    
+    
+    
+    context 'bool:' do
+      it 'just one' do
+        data = "bool"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("bool")
+      end
+    
+      it 'multiple' do
+        data = "boolbool bool"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("bool bool bool")
+      end
+      
+      it 'with partials' do
+        data = "bbobooboolbozboox"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("bool")
+      end
+    end
+    
+    
+    
+    context 'cos:' do
+      it 'just one' do
+        data = "cos"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("cos")
+      end
+    
+      it 'multiple' do
+        data = "coscos cos"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("cos cos cos")
+      end
+      
+      it 'with partials' do
+        data = "ccocosczcox"
+        expect(Parser.parse(data).map{|t| t.to_s}.join(" ")).
+        to eq("cos")
+      end
+    end
+    
   end
 end
