2dce447de051d0260a0088682388cc73dec122a4
Woffendin, Michael Harrison <michael.woffendin@oregonstate.edu>
2015-02-15 22:37:58 -0800

    Get it to parse with constants

---
+++ transitions.rb
@@ -0,0 +1,101 @@
+
+
+class Transition
+  def self.token?
+    false
+  end
+  def self.has_first? token
+    self.firsts.include? token
+  end
+  def self.firsts
+    return self.options.map{|o|o.first.firsts}.flatten
+  end
+  def self.transit token
+    self.options.map{|o|o.first.firsts.include?(token)}.index(true)
+  end
+end
+
+
+class Empty < Token
+  def self.firsts
+    return [Empty]
+  end
+end
+
+
+class Start < Transition
+  def self.firsts
+    return LeftParen, Id, MInteger, MString, MReal, MBoolean
+  end
+
+  def self.options
+    return [[LeftParen, Start3], [Expr, Start2]]
+  end
+end
+
+
+class Start2 < Transition
+  def self.options
+    return [[Start, Start2], [Empty]]
+  end
+end
+
+
+class Start3 < Transition
+  def self.options
+    return [[RightParen, Start2], [Start, RightParen, Start2]]
+  end
+end
+
+
+class Expr < Transition
+  def self.options
+    return [[Oper], [Stmts]]
+  end
+end
+
+
+class Stmts < Transition
+  def self.firsts
+    return []
+  end
+  def self.options
+    return [[Stmts]]
+  end
+end
+
+class Binops < Transition
+  def self.firsts
+    return []
+  end
+  def self.options
+    return [[Binops]]
+  end
+end
+
+class Unops < Transition
+  def self.firsts
+    return []
+  end
+  def self.options
+    return [[Unops]]
+  end
+end
+
+class Oper < Transition
+  def self.options
+    return [[LeftParen, Assign, Id, Oper], [LeftParen, Binops, Oper, Oper], [LeftParen, Unops, Oper], [Constant], [Id]]
+  end
+end
+
+class Constant < Transition
+  def self.options
+    return [[MInteger], [MReal], [TString]]
+  end
+end
+
+class TString < Transition
+  def self.options
+    return [[MString], [MBoolean]]
+  end
+end
